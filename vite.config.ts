e51cc0e087556b1fb9069eee31ed7b4a520101d9
Thanakorn Phuttharaksa <crossdeltavi@gmail.com>
2026-07-28 19:46:40 +0700

    fixed vite custom chunking causing gh page to fail

--- vite.config.ts
+++ vite.config.ts
@@ -8,20 +8,5 @@ export default defineConfig({
   optimizeDeps: {
     exclude: ["react-latex-next"],
   },
-  build: {
-    rollupOptions: {
-      output: {
-        manualChunks: (id) => {
-          if (id.includes("node_module")) {
-            return id
-              .toString()
-              .split("node_modules/")[1]
-              .split("/")[0]
-              .toString();
-          }
-        },
-      },
-    },
-  },
   resolve: { tsconfigPaths: true },
 });
